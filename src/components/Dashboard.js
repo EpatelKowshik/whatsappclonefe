c7a8d575ab9fb05d4fd1edd9afd6085abe545b12
EpatelKowshik <88603935+EpatelKowshik@users.noreply.github.com>
2021-08-22 08:54:53 +0530

    navbar feature

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,14 +2,28 @@ import React from 'react'
 import Sidebar from './Sidebar';
 import OpenConversation from './OpenConversation';
 import { useConversations } from '../contexts/ConversationsProvider';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 export default function Dashboard({ id }) {
   const { selectedConversation } = useConversations()
 
   return (
+    <>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand>
+          MessagingApp
+        </Navbar.Brand>
+        <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
     <div className="d-flex" style={{ height: '100vh' }}>
       <Sidebar id={id} />
       {selectedConversation && <OpenConversation />}
     </div>
+    </>
   )
 }
